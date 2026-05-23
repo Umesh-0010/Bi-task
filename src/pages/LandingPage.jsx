b9b33553bcfb207454f275e7b8a7c531a5889810
Umesh <usarki674@gmail.com>
2026-05-23 18:57:37 +0530

    fix hero slider width secont time

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,24 +2,21 @@ import React from "react";
 import Notice from "../components/Notice";
 import TopBar from "../components/TopBar";
 import Hero from "../components/Hero";
-import Hero1 from "../components/hero1";
 import Liderprofile from "../components/Liderprofile";
 import Post from "../components/Post";
 import Posts2 from "../components/Post2";
 import FooterComponent from "../components/FooterComponent";
 
-
 function LandingPage() {
   return (
     <div className="bg-[#0A5F85]">
-      {/* <Notice />
-      <TopBar /> */}
-      <Hero /> 
-      {/* <Hero1/> */}
+      <Notice />
+      <TopBar />
+      <Hero />
       <Liderprofile />
-      <Post/>
-      <Posts2/>
-      <FooterComponent/>
+      <Post />
+      <Posts2 />
+      <FooterComponent />
     </div>
   );
 }
